ab6308989560d6f7d5fddfd1c741e4540ac1f183
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-12-17 15:34:59 +0200

    get-customers modified

--- api/get-customers/index.ts
+++ api/get-customers/index.ts
@@ -2,14 +2,16 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-        let tuloste = JSON.stringify(context.bindings.inputDocument);
+    let tuloste = "";
+    try {
+        tuloste = JSON.stringify(context.bindings.inputDocument[0].customerlist);
+    } catch (error) {
+        context.log("Stringifying customerlist failed, error: " + error);
+    }
+    
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseMessage + " " + tuloste
+        body: tuloste
     };
 
 };
