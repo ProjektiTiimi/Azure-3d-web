1fefb74d7778e7f12b4f6026602963f638c48f76
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2021-12-21 10:41:15 +0200

    addCustomer modified

--- api/add-customer/index.ts
+++ api/add-customer/index.ts
@@ -3,23 +3,24 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
-    let id = "1584912f-41f8-48e7-b721-6b3fe32482ff"
+    let idocument = context.bindings.inputDocument;
 
-    context.bindings.outputDocument = JSON.stringify({
-        YTunnus: req.body.YTunnus,
-        asiakkaanNimi: req.body.asiakkaanNimi,
-        Postitusosoite: req.body.Postitusosoite,
-        Postinumero: req.body.Postinumero,
-        Toimipaikka: req.body.Toimipaikka
-    });
+    let newcustomer = req.body;
+
+    newcustomer.id = Date.now();
+
+    idocument[0].customerlist.push(newcustomer);
+
     let message = ("req.body: " + JSON.stringify(req.body));
+
+    context.bindings.outputDocument = idocument;
     
 
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: message
     };
-
+ 
 };
 
 export default httpTrigger;
