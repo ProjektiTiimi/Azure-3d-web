3b5c580f208a77e566ccb57a566898fb960b9d75
JoonaMartinkauppi <61461699+JoonaMartinkauppi@users.noreply.github.com>
2022-01-03 11:50:05 +0200

    Update Homepage.tsx

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -8,7 +8,7 @@ const Homepage: React.FunctionComponent = () => {
             <h1>Tervetuloa laskut sovellukseen {CurrentUser}</h1>
             <h3>
             </h3>
-            <a href="/.auth/login/aad">Login</a>
+            <a href="/.auth/login/aad?post_login_redirect_url=https://wonderful-sea-0b9eeac03.azurestaticapps.net/">Login</a>
         </div>
     )
 };
